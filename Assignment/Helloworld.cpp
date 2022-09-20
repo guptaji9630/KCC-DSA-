7c9431b7cd68c6d4e9eb9dfd9e92afa4e060b564
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2022-09-20 20:03:21 +0530

    Assignment 1

---
+++ Assignment/Helloworld.cpp
@@ -0,0 +1,8 @@
+// Hackerrank - https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/289143723
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+   cout<<"Hello, World!";
+}
