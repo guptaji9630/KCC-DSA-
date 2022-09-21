fc27658594b0cd11930828072d071025f813db88
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2022-09-21 10:30:31 +0530

    Assignment 1

---
+++ Assignment/Input_and_Output.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+      int a,b,c;
+      cin>>a>>b>>c;
+      int sum = a+b+c;
+      cout<<sum;
+      
+    //   cout<<a+n+p<<endl;
+      
+    //   cout<<s<<""<<a+n+a<<endl;
+      
+    return 0;
+}
