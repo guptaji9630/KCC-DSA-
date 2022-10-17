9a68a013999ec7f55daadd2355a60b1116f6549f
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2022-10-17 21:20:34 +0530

    Create Inserting a Node Into a Sorted Doubly Linked List.cpp

---
+++ Assignment 5/Inserting a Node Into a Sorted Doubly Linked List.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'sortedInsert' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_DOUBLY_LINKED_LIST llist
+ *  2. INTEGER data
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+DoublyLinkedListNode *newNode = new DoublyLinkedListNode(data);
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        if (head == NULL)
+        {
+            return newNode;
+        }
+        if (head->data >= newNode->data)
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            return newNode;
+        }
+        DoublyLinkedListNode *current = head;
+        while (current->next != NULL && current->next->data < newNode->data)
+        {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        if (current->next != NULL)
+        {
+            newNode->next->prev = newNode;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+        return head;
+}
+
+int main()
